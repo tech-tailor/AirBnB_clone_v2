0a4a2e53e8993c68943fd76b8b01c97882705eb2
Akin <ogundareakinniyi8@gmail.com>
2024-01-16 00:26:12 +0100

    setup_mysql_test.sql

---
+++ setup_mysql_test.sql
@@ -0,0 +1,11 @@
+-- create user, database and grant priviledges 
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
+IDENTIFIED BY 'hbnb_test_pwd';
+
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
