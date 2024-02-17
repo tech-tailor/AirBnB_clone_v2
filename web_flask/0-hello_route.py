838cb10492a0fceca67dfaa6d791033bf9f767ca
Akin <ogundareakinniyi8@gmail.com>
2024-02-17 03:49:39 +0100

    added readme and 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """
     simple flask to display hello HBNB
     """
     return "Hello HBNB!"
+
+
 app.run(host="0.0.0.0", port=5000)
