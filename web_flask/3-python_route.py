ed509c54f5d34ba300d8645f9ed71a95488df5d2
Akin <ogundareakinniyi8@gmail.com>
2024-02-17 14:08:54 +0100

    added 3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    simple flask to display hello HBNB
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """
+    simple flask to display hello HBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_text(text):
+    """
+    simple flask to display hello HBNB
+    """
+    formatted_text = text.replace('_', ' ')
+    return f"C {formatted_text}"
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python_text(text='is cool'):
+    """
+    simple flask to display hello HBNB
+    """
+    formatted_text = text.replace('_', ' ')
+    return f"Pyhon {formatted_text}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
